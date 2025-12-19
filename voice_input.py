423e544cf7e82bb4ce3f264c4429c95e402be6e8
devigangatharan-netizen <devigangatharan@gmail.com>
2025-12-19 22:41:13 +0530

    Modify get_voice_input to disable voice recognition

--- voice_input.py
+++ voice_input.py
@@ -1,12 +1,2 @@
-import speech_recognition as sr
-
 def get_voice_input():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio = r.listen(source)
-
-    try:
-        return r.recognize_google(audio)
-    except:
-        return ""
+    return "Voice input is not supported on cloud servers."
